707e086d5568a9b5a58fcc7396bc0ecd5b232e30
James Rak <jamesm.villeza@gmail.com>
2021-06-12 17:25:42 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "@atlaskit/css-reset";
+import {DragDropContext} from "react-beautiful-dnd";
+import initialData from "./initial-data";
+import Column from "./column";
+
+class App extends React.Component {
+    state = initialData;
+
+    onDragEnd = (result) => {
+        //
+    };
+    render() {
+        return (
+            <DragDropContext onDragEnd={this.onDragEnd}>
+                {this.state.columnOrder.map((columnId) => {
+                    const column = this.state.columns[columnId];
+                    const tasks = column.taskIds.map(
+                        (taskId) => this.state.tasks[taskId]
+                    );
+
+                    return (
+                        <Column key={column.id} column={column} tasks={tasks} />
+                    );
+                })}
+            </DragDropContext>
+        );
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
